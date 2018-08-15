a6311ad206aba8deb94ad0f835b5e288faef81ce
krishnareddy420 <mkrishnamca88@gmail.com>
2018-08-15 16:59:51 +0530

    Initial checkin

---
+++ accounts-microservice-server-master/src/main/java/com/doj/ms/accounts/AccountsMicroserviceServerApplication.java
@@ -0,0 +1,14 @@
+package com.doj.ms.accounts;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsMicroserviceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsMicroserviceServerApplication.class, args);
+	}
+	
+}
